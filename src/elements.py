e6a31efcfef80cfec94e9239ed91102570bb627b
andrewolesen <andrewolesen852@gmail.com>
2023-08-21 17:31:12 -0600

    Initial commit

---
+++ src/elements.py
@@ -0,0 +1,12 @@
+class Snake:
+    def __init__(self) -> None:
+        pass
+
+class Apple:
+    def __init__(self) -> None:
+        pass
+
+class Game:
+    def __init__(self) -> None:
+        pass
+    
